dcf6c3c1e47576ddd6116a4d226151659f76a47d
Paul <prdickson@gmail.com>
2020-12-26 11:28:53 +0000

    first

---
+++ week4/ShannonEntropy.java
@@ -0,0 +1,22 @@
+public class ShannonEntropy {
+    public static void main(String[] args) {
+        int m = Integer.parseInt(args[0]);
+        int n = 0;
+        int[] occ = new int[m];
+
+        while (!StdIn.isEmpty()) {
+            int x = StdIn.readInt();
+            occ[x-1] += 1;
+            n += 1;
+        }
+
+        double ent = 0.0;
+        for(int i = 0; i < m; i++) {
+            if (occ[i] > 0) {
+                double p = (double)occ[i] / n;
+                ent += p * (Math.log(p) / Math.log(2));
+            }
+        }
+        StdOut.println(-ent);
+    }
+}
