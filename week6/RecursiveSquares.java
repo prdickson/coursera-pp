ce72bb9c51c69ae7ad57cb9fdb3ff96d0a34ccff
Paul Dickson <3018673+prdickson@users.noreply.github.com>
2021-01-30 11:28:33 +0000

    finished sq

--- week6/RecursiveSquares.java
+++ week6/RecursiveSquares.java
@@ -16,14 +16,22 @@ public class RecursiveSquares {
     // Draws a recursive square pattern of order n, centered on (x, y)
     // of the given side length.
     public static void draw(int n, double x, double y, double length) {
+        if (n == 0) return;
+        double halfLength = length / 2;
+        draw(n - 1, x - halfLength, y + halfLength, halfLength);
+        draw(n - 1, x + halfLength, y + halfLength, halfLength);
         drawSquare(x, y, length);
+        draw(n - 1, x - halfLength, y - halfLength, halfLength);
+        draw(n - 1, x + halfLength, y - halfLength, halfLength);
     }
 
     // Takes an integer command-line argument n and draws a recursive
     // square pattern of order n, centered on (0.5, 0.5) with side length 0.5.
     public static void main(String[] args) {
+        StdDraw.enableDoubleBuffering();
         int n = Integer.parseInt(args[0]);
         StdDraw.setScale(0, 1);
         draw(n, 0.5, 0.5, 0.5);
+        StdDraw.show();
     }
 }
