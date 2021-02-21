41e6f86a0e8719c0b74200a8dd78c04b987d768a
Paul Dickson <3018673+prdickson@users.noreply.github.com>
2021-02-21 20:18:57 +0000

    Added Ramanujan

--- week7/Ramanujan.java
+++ week7/Ramanujan.java
@@ -1,14 +1,31 @@
+import org.w3c.dom.xpath.XPathResult;
+
 public class Ramanujan {
     // Is n a Ramanujan number?
     public static boolean isRamanujan(long n) {
-        return false;
+        long hi = (long) Math.cbrt(n);
+        long mid  = n / 2;
+        int pairsFound = 0;
+
+        for (long i = 1; i < hi; i++) {
+            long a = i * i * i;
+
+            if (a > mid)
+                break;
+
+            double b = Math.cbrt(n - a);
+
+            if (b % 1 == 0)
+                pairsFound++;
+        }
+
+        return pairsFound == 2;
     }
 
     // Takes a long integer command-line arguments n and prints true if
     // n is a Ramanujan number, and false otherwise.
     public static void main(String[] args) {
-        int n = Integer.parseInt(args[0]);
-
+        long n = Long.parseLong(args[0]);
         StdOut.print(isRamanujan(n));
         StdOut.println();
     }
