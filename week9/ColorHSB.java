d46ee500dbe7215ffb865139f5d12f2146df9f9c
Paul Dickson <3018673+prdickson@users.noreply.github.com>
2021-03-20 13:08:21 +0000

    Added validation

--- week9/ColorHSB.java
+++ week9/ColorHSB.java
@@ -1,7 +1,12 @@
 public class ColorHSB {
     // Creates a color with hue h, saturation s, and brightness b.
     public ColorHSB(int h, int s, int b) {
-
+        if (h < 0) throw new IllegalArgumentException("h");
+        if (h > 359) throw new IllegalArgumentException("h");
+        if (s < 0) throw new IllegalArgumentException("s");
+        if (s > 100) throw new IllegalArgumentException("s");
+        if (b < 0) throw new IllegalArgumentException("b");
+        if (b > 100) throw new IllegalArgumentException("b");
     }
 
     // Returns a string representation of this color, using the format (h, s, b).
--- week9/ColorHSBTest.java
+++ week9/ColorHSBTest.java
@@ -1,2 +1,41 @@
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
 public class ColorHSBTest {
+    @Test
+    public void testThrowsWhenHueIsLessThanZero() {
+        new ColorHSB(0, 0, 0); //does not throw
+        assertThrows(IllegalArgumentException.class, () -> new ColorHSB(-1, 0, 0));
+    }
+
+    @Test
+    public void testThrowsWhenHueIsGreaterThan359() {
+        new ColorHSB(339, 0, 0); //does not throw
+        assertThrows(IllegalArgumentException.class, () -> new ColorHSB(360, 0, 0));
+    }
+
+    @Test
+    public void testThrowsWhenSaturationIsLessThanZero() {
+        new ColorHSB(0, 0, 0); //does not throw
+        assertThrows(IllegalArgumentException.class, () -> new ColorHSB(0, -1, 0));
+    }
+
+    @Test
+    public void testThrowsWhenSaturationIsGreaterThan100() {
+        new ColorHSB(0, 100, 0); //does not throw
+        assertThrows(IllegalArgumentException.class, () -> new ColorHSB(0, 101, 0));
+    }
+
+    @Test
+    public void testThrowsWhenBrightnessIsLessThanZero() {
+        new ColorHSB(0, 0, 0); //does not throw
+        assertThrows(IllegalArgumentException.class, () -> new ColorHSB(0, 0, -1));
+    }
+
+    @Test
+    public void testThrowsWhenBrightnessIsGreaterThan100() {
+        new ColorHSB(0, 0, 0); //does not throw
+        assertThrows(IllegalArgumentException.class, () -> new ColorHSB(0, 0, 101));
+    }
 }
