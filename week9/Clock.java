c987561cf6c05305c4c5bbddd8447250fdc76056
Paul Dickson <3018673+prdickson@users.noreply.github.com>
2021-03-21 09:20:54 +0000

    Add ctors and to string

--- week9/Clock.java
+++ week9/Clock.java
@@ -1,21 +1,41 @@
 public class Clock {
 
+    private int h;
+    private int m;
+
     // Creates a clock whose initial time is h hours and m minutes.
     public Clock(int h, int m) {
-        if (h < 0) throw new IllegalArgumentException("h");
-        if (h > 23) throw new IllegalArgumentException("h");
-        if (m < 0) throw new IllegalArgumentException("m");
-        if (m > 59) throw new IllegalArgumentException("m");
+        setTime(h, m);
     }
 
     // Creates a clock whose initial time is specified as a string, using the format HH:MM.
     public Clock(String s) {
+        String[] parts = s.split(":");
 
+        if (parts.length != 2) throw new IllegalArgumentException("s");
+        if (parts[0].length() != 2) throw new IllegalArgumentException("s");
+        if (parts[1].length() != 2) throw new IllegalArgumentException("s");
+
+        try {
+            setTime(Integer.parseInt(parts[0]), Integer.parseInt(parts[1]));
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException("s");
+        }
+    }
+
+    private void setTime(int h, int m) {
+        if (h < 0) throw new IllegalArgumentException("h");
+        if (h > 23) throw new IllegalArgumentException("h");
+        if (m < 0) throw new IllegalArgumentException("m");
+        if (m > 59) throw new IllegalArgumentException("m");
+
+        this.h = h;
+        this.m = m;
     }
 
     // Returns a string representation of this clock, using the format HH:MM.
     public String toString() {
-        return "";
+        return String.format("%02d:%02d", h, m) ;
     }
 
     // Is the time on this clock earlier than the time on that one?
--- week9/ClockTest.java
+++ week9/ClockTest.java
@@ -15,4 +15,42 @@ public class ClockTest {
         assertThrows(IllegalArgumentException.class, () -> new Clock(0, -1));
         assertThrows(IllegalArgumentException.class, () -> new Clock(0, 60));
     }
+
+    @Test
+    public void testThrowsWhenInstantiatedWithStringWithoutColon() {
+        assertThrows(IllegalArgumentException.class, () -> new Clock("hey"));
+    }
+
+    @Test
+    public void testThrowsWhenInstantiatedWithStringWithThreeParts() {
+        assertThrows(IllegalArgumentException.class, () -> new Clock("hey:hey:hey"));
+    }
+
+    @Test
+    public void testThrowsWhenInstantiatedWithStringNonNumericHours() {
+        assertThrows(IllegalArgumentException.class, () -> new Clock("hey:10"));
+    }
+
+    @Test
+    public void testThrowsWhenInstantiatedWithStringNonNumericMinutes() {
+        assertThrows(IllegalArgumentException.class, () -> new Clock("10:hey"));
+    }
+
+    @Test
+    public void testThrowsWhenInstantiatedWithInvalidTimeString() {
+        assertThrows(IllegalArgumentException.class, () -> new Clock("-01:00"));
+        assertThrows(IllegalArgumentException.class, () -> new Clock("24:00"));
+        assertThrows(IllegalArgumentException.class, () -> new Clock("00:-01"));
+        assertThrows(IllegalArgumentException.class, () -> new Clock("00:60"));
+    }
+
+    @Test
+    public void testToStringWithoutPadding() {
+        assertEquals("12:34", new Clock(12, 34).toString());
+    }
+
+    @Test
+    public void testToStringWithPadding() {
+        assertEquals("01:02", new Clock(1, 2).toString());
+    }
 }
